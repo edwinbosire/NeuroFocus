f0d10743c51cec480689b40080c115d425c8cc7c
Edwin Bosire <me@edwinb.co.uk>
2025-11-21 00:37:35 +0000

    Initial Commit

---
+++ NeuroFocus/NeuroFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroFocusApp.swift
+//  NeuroFocus
+//
+//  Created by Edwin Bosire on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NeuroFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeuroFocusTests/NeuroFocusTests.swift
@@ -0,0 +1,16 @@
+//
+//  NeuroFocusTests.swift
+//  NeuroFocusTests
+//
+//  Created by Edwin Bosire on 21/11/2025.
+//
+
+import Testing
+
+struct NeuroFocusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
